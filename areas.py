786e7572b65a0cc00867301e2b8fd67da827306c
Chance03-git <Chancefoster05@gmail.com>
2024-11-22 17:19:50 -0500

    documentation for rectangle area

--- areas.py
+++ areas.py
@@ -1,7 +1,13 @@
 def rectangle_area(length, width):
+    """Calculates the area of a rectangle.
+
+    Args:
+        length: The length of the rectangle.
+        width: The width of the rectangle.
+
+    Returns:
+        The area of the rectangle (length × width).
+    """
     return length * width
-
-
-
 def test_should_return_rectangle_area():
     assert  rectangle_area(5, 3) == 15
